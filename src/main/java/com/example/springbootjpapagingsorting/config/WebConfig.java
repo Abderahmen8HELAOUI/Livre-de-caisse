5b6d50ed184e87c46885c74d4dfc327130a66baf
DhiaddineHl <hlaouidhia@gmail.com>
2023-09-27 16:12:16 +0100

    add config

---
+++ src/main/java/com/example/springbootjpapagingsorting/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.example.springbootjpapagingsorting.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author USER
+ * @version 1.0
+ * @created 27/09/2023 - 14:26
+ * @project spring-boot-jpa-paging-sorting
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins(" https://comptabilitequotidienneangular-337070756a36.herokuapp.com")
+                .allowedMethods("*")
+                .maxAge(3600L)
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
